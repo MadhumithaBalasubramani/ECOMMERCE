1444c6b451580036b619dd331d7e86c045a55eaf
MadhumithaBalasubramani <madhubalu2082001@gmail.com>
2024-05-20 12:02:59 +0530

    fghjkl

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Navbar from './Components/Navbar';
+import Home from './Pages/home';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    </>
+  );
+}
+
+export default App;
