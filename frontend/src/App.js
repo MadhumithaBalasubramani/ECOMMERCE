29f95ca2173eb3e8e05defc1de0a6fc1e87d093e
janubharathi <bharathisankarias@gmail.com>
2024-05-21 17:22:46 +0530

    njjk

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,16 +13,15 @@ function App() {
     <Navbar/>
     
     <Routes>   
-      <Route path="/shop" element={<Shop/>}/>
+      {/* <Route path="/shop" element={<Shop/>}/>
       <Route path="/men" element={<Men/>}/>
       <Route path="/women" element={<Women/>}/>
-      <Route path="/kids" element={<Kids/>}/>
+      <Route path="/kids" element={<Kids/>}/> */}
       <Route path="/login" element={<LoginSignup/>}/>
       <Route path="/cart" element={<Cart/>}/>
-<<<<<<< HEAD
+
       
-=======
->>>>>>> 5e43900665816827c9f7115f96e96ba3cdbad237
+
     </Routes> 
     </>
   )
