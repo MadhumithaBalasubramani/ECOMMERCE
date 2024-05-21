fa6fb2f0dbdcf1f143c4db6cfd3a18b95fc86177
janubharathi <bharathisankarias@gmail.com>
2024-05-21 12:58:03 +0530

    ghjk

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Home from './Pages/home';
 
+
 function App() {
   return (
     <>
       <Home/>
+      
     </>
   );
 }
--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const LoginSignup=()=> {
   return (
-    <div>LoginSignup</div>
+    <div className='loginsignup'>
+        <div className="loginsignup-container">
+            <h1>Sign Up</h1>
+            <div className="loginsignup-fields">
+                <input type="text" placeholder='Your Name'/>
+                <input type="text" placeholder='Email Address' />
+                <input type="password" placeholder='password' />
+            </div>
+            <button>Continue</button>
+            <p className="loginsignup-login">Already have an account <span >Login here </span ></p>
+            <div className="loginsignup-agree">
+                <input typ="checkbox" name='' id=''/>
+                <p>By continuing, i agree to the terms of use & privacy policy.</p>
+            </div>
+
+
+        </div>
+    </div>
   )
 }
 
